8ce30c5b39604f33cb3efac0fe53b766f3b60a84
Оля Труш <101470663+OlyaTrush@users.noreply.github.com>
2023-12-20 23:09:27 +0200

    9

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App";
+import App from "./App";
 
-import theme from "./src/theme";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
